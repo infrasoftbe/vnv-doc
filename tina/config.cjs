852dd5d672dd0bf8faf87ebb475c8eaadc20ce1a
MortallicaXxX <47045327+MortallicaXxX@users.noreply.github.com>
2025-09-25 11:38:38 +0200

    update

---
+++ tina/config.cjs
@@ -0,0 +1,76 @@
+const { defineConfig } = require('tinacms');
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  'main';
+
+module.exports = defineConfig({
+  branch,
+  clientId: '8a60293c-003f-4349-af80-cdb80b56c58b', // Get this from tina.io
+  token: '181ac6e95fae4193a1e67014dc374fc569f655a4', // Get this from tina.io
+  build: {
+    outputFolder: 'admin',
+    publicFolder: 'public',
+  },
+  media: {
+    tina: {
+      mediaRoot: 'images',
+      publicFolder: 'public',
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: 'guides',
+        label: 'Doc - Guides',
+        path: '/content/docs/guides',
+        format: 'md',
+        fields: [
+          {
+            type: 'string',
+            name: 'title',
+            label: 'Title',
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body',
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: 'posts',
+        label: 'Posts',
+        path: 'content/posts',
+        format: 'md',
+        fields: [
+          {
+            type: 'string',
+            name: 'title',
+            label: 'Title',
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: 'datetime',
+            name: 'posted',
+            label: 'Date Posted',
+            required: true,
+          },
+          {
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body',
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
